69965c8c1c7cf9eaada007d6f8c99cfffbb72081
rjtprasad <prasadrajat28@gmail.com>
2025-03-12 22:15:11 +0530

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 export default function LandingPage() {
   return (
-    <div className="bg-slate-50">
+    <div className="bg-slate-50 grainy-light">
       {/* Hero Section */}
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
